d27f8becb97b8bafcda182de73c851a3f373febf
Carsten Witzke <carsten@staticline.de>
2013-02-26 23:43:37 +0100

    cleanup
    renamed script
    replaced optparse with argparse
    
    Signed-off-by: Carsten Witzke <carsten@staticline.de>

--- spring.py
+++ spring.py
@@ -8,18 +8,19 @@ based on pyapns
 @see https://github.com/samuraisam/pyapns
 '''
 
+from SocketServer import TCPServer
 from optparse import OptionParser
 from pyapns import configure, provision
+from pyapns.client import notify
+import argparse
+import logging
 import os
 import pyapns._json
 import pyapns.server
 import twisted.application.internet
 import twisted.web
 
-from pyapns.client import notify
-from SocketServer import TCPServer
 
-import logging
 
 logging.basicConfig()
 log = logging.getLogger('spring.logger')
@@ -55,15 +56,9 @@ def init_twisted():
     resource.putChild('', service)
     site = twisted.web.server.Site(resource)
     
-    ## FIXME: currently broken 
-    ##server = TCPServer(port, site)
-    ##server.setServiceParent(application)
-    
-    print '''#######################
-    Auto-launch of twisted currently not implemented!
-    Please start twisted instance manually: 
-    $ twistd -r default web --class=pyapns.server.APNSServer --port=7077  #simple example
-#######################'''
+    server = TCPServer(port, site)
+    server.setServiceParent(application)
+
 
 def got_feedback(tuples):
     if not tuples: return
@@ -71,29 +66,32 @@ def got_feedback(tuples):
     for t in tuples:
         log.debug('%s - %s' % (t[0],t[1]))
     
+    
 def got_error(error):
     log.error('## Error handler')
     log.error(error)
 
+
 def push(app_id, message=None, badge=0, sound=None, custom=None):
-    # FIXME: init and push merged in one function - bad!
-    #init_twisted() # under construction
-    
+    # FIXME: init service and push merged in one function - bad!
+    #init_twisted()
+    log.warn('auto-init of service currently not realiable; use manual start!')
+        
     with open(os.path.abspath(config_file)) as f:
         config = pyapns._json.loads(f.read())
     
-    ####
-    configure({'HOST': 'http://localhost:7077/'})
-    if 'autoprovision' in config:
-        for app in config['autoprovision']:
-            app_id = app['app_id']
-            if app_id == app_id and os.path.exists(app['cert']):
-                log.debug('found %s' % app_id)
-                provision(app['app_id'], open(app['cert']).read(), app['environment'])
-            else:
-                log.error('no file at %s found!' % app['cert'])
-    ####
-    pyapns.feedback(app_id, async=True, callback=got_feedback, errback=got_error)
+    ## configure on the fly
+    if pyapns.client.OPTIONS['CONFIGURED'] == False:
+        configure({'HOST': 'http://localhost:7077/'})
+        if 'autoprovision' in config:
+            for app in config['autoprovision']:
+                app_id = app['app_id']
+                if app_id == app_id and os.path.exists(app['cert']):
+                    log.debug('found %s' % app_id)
+                    provision(app['app_id'], open(app['cert']).read(), app['environment'])
+                else:
+                    log.error('no file at %s found!' % app['cert'])
+        pyapns.feedback(app_id, async=True, callback=got_feedback, errback=got_error)
     
     ## create notification
     notification = {'aps':{}}
@@ -115,34 +113,27 @@ def push(app_id, message=None, badge=0, sound=None, custom=None):
 
 
 if __name__ == '__main__':
-    parser = OptionParser(version='spring v1.0')
-    parser.add_option('-a', '--alert', dest='alert', help='alert text')
-    parser.add_option('-b', '--badge', dest='badge', help='badge number; \'0\' if you want to clear the app\'s current badge')
-    parser.add_option('-s', '--sound', dest='sound', help='sound file')
-    parser.add_option('-c', '--custom', dest='custom', help='custom key/value pairs')
-    parser.add_option('-i', '--id', dest='app_id', help='application ID, if you have multiple apps registered')
-    ## handle arguments
-    (options, args) = parser.parse_args()
-    if len(args) == 4:
-        options.alert = args[0]
-        options.badge = args[1]
-        options.sound = args[2]
-        options.custom = args[3]
-    elif len(args) == 3:
-        options.alert = args[0]
-        options.badge = args[1]
-        options.sound = args[2]
-    elif len(args) == 2:
-        options.alert = args[0]
-        options.badge = args[1]
-    elif len(args) == 1:
-        options.alert = args[0]
-    else:
-        parser.error("incorrect number of arguments")
-        
+    parser = argparse.ArgumentParser(prog='spring', description='Triggers iOS push notifications.')
+    parser.add_argument('-a', '--alert', type=str, dest='alert', 
+                        help='alert text')
+    parser.add_argument('-b', '--badge', type=int, dest='badge', 
+                        help='badge number; \'0\' if you want to clear the app\'s current badge')
+    parser.add_argument('-s', '--sound', type=str, dest='sound', 
+                        help='sound file')
+#    parser.add_argument('-c', '--custom', type=dict, dest='custom', 
+#                        help='custom key/value pairs')
+    parser.add_argument('-i', '--id', dest='app_id', 
+                        help='application ID, if you have multiple apps registered')
+    parser.add_argument('--version', action='version', version='spring v1.0')
+    args = parser.parse_args()
+    
+    ## app id is required
+    if args.app_id == None:
+        parser.error('must define target application')
+    
     ## add file extension to sound file if missing
-    if options.sound and options.sound.find('.caf') == -1:
-        options.sound += '.caf'
+    if args.sound and args.sound.find('.caf') == -1:
+        args.sound += '.caf'
         
-    log.debug('selected app: %s' % options.app_id)
-    push(options.app_id, options.alert, options.badge, options.sound, options.custom)
+    ## TODO: handle custom key/value pairs (pickle, json, ...)
+    push(args.app_id, args.alert, args.badge, args.sound, None)
